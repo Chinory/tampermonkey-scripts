27c8df69d2145cd21b73851713b59004a8c8e075
Chinory <chinory@cup.moe>
2021-01-20 07:28:00 +0800

    support window.open() links

--- click-to-qliveplayer.js
+++ click-to-qliveplayer.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Click to QLivePlayer
-// @version      0.1.0
+// @version      0.2.0
 // @description  Open QLivePlayer instead when click on video link in BiliBili
 // @author       Chinory
 // @homepage     https://github.com/Chinory/tampermonkey-scripts/wiki/click-to-qliveplayer.js
@@ -8,29 +8,44 @@
 // @grant        unsafeWindow
 
 // ==/UserScript==
-'use strict'
+"use strict";
 
-window.addEventListener('click', function (e) {
-  var a = e.target.closest('a')
-  if (a == null) return
-  var l = new URL(a.href)
-  var s = l.pathname
-  var p
+var open = unsafeWindow.open;
+
+function rewrite(l) {
+  var s = l.pathname;
   switch (l.hostname) {
-    case 'www.bilibili.com': {
-      if (s.slice(0, 7) !== '/video/' &&
-          s.slice(0, 14) !== '/bangumi/play/') return
-      p = (p = l.searchParams.get('p')) ? '?p=' + p : ''
-      break
-    }
-    case 'live.bilibili.com': {
-      if (!/^\/\d+$/.test(s)) return
-      p = ''
-      break
-    }
+    case "www.bilibili.com":
+      if (s.slice(0, 7) === "/video/" || s.slice(0, 14) === "/bangumi/play/") {
+        var p = l.searchParams.get("p");
+        if (p) s = s + "?p=" + p;
+        break;
+      } else return;
+    case "live.bilibili.com":
+      if (/^\/\d+$/.test(s)) break;
+      else return;
     default:
-      return
+      return;
   }
-  this.open('qliveplayer://' + l.host + s + p, '_self')
-  return e.preventDefault()
-}, true)
+  return "qliveplayer://" + l.host + s;
+}
+
+unsafeWindow.addEventListener(
+  "click",
+  function (e) {
+    var a, l, u;
+    (a = e.target.closest("a")) &&
+      (l = new URL(a.href, this.location.href)) &&
+      (u = rewrite(l)) &&
+      open.call(this, u, "_self") &&
+      e.preventDefault();
+  },
+  true
+);
+
+unsafeWindow.open = function (a) {
+  var l = new URL(a, this.location.href);
+  var u = rewrite(l);
+  if (u) return open.call(this, u, "_self");
+  else return open.apply(this, arguments);
+};
